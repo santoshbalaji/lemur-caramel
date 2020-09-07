b9b952cdc6262524807c1dff49c1d813f78714b5
santoshbalaji <santechselva@gmail.com>
2020-09-07 21:57:52 +0800

    Add mqtt layer codes

--- server-backend/main.py
+++ server-backend/main.py
@@ -1,13 +1,20 @@
 from flask import Flask
+import paho.mqtt.client as mqtt
 from app import caramel_blueprint
+from constants import FLASK_HOST, FLASK_PORT
 
 
 def start_server():
     app = Flask(__name__)
     app.register_blueprint(caramel_blueprint)
     app.config['ERROR_404_HELP'] = False
-    app.run(host='127.0.0.1', port='3414', debug=True)
+    app.run(host=FLASK_HOST, port=FLASK_PORT, debug=True)
+
+
+def start_scheduler():
+    client = mqtt.Client()
 
 
 if __name__ == '__main__':
     start_server()
+    start_scheduler()
